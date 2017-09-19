24dc155a64f0020531fec5e4e41aa03eee4c614d
Max Jackson <mjacksoncfa@gmail.com>
2017-09-18 22:26:53 -0400

    fix: hid tracks nav item

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { Root, Drawer, View, Icon, Text, H3 } from 'native-base/src';
 import { StackNavigator, DrawerNavigator, DrawerItems } from 'react-navigation';
-import Feed from './Feed';
-import Talk from './Talk';
+// import Feed from './Feed';
+// import Talk from './Talk';
 import Agenda from './Agenda';
 import About from './About';
 import Contact from './Contact';
-import MyPycon from './MyPycon';
+// import MyPycon from './MyPycon';
 import BrandedContainer from './BrandedContainer';
 
-const FeedNavigator = StackNavigator({
+/* const FeedNavigator = StackNavigator({
     Tracks: { screen: Feed },
     Talk: { screen: Talk }
 }, {
     initialRouteName: 'Tracks'
-});
+}); */
 
 const AgendaNavigator = StackNavigator({
     Agenda: { screen: Agenda }
@@ -36,12 +36,12 @@ const AboutNavigator = StackNavigator({
 const AppNavigator = DrawerNavigator({
     // Home: { screen: Home },
     Agenda: { screen: AgendaNavigator },
-    Tracks: { screen: FeedNavigator },
+    // Tracks: { screen: FeedNavigator },
     // MyPycon: { screen: MyPyconNavigator },
     About: { screen: AboutNavigator },
     Contact: { screen: ContactNavigator }
 }, {
-    initialRouteName: 'Tracks',
+    initialRouteName: 'Agenda',
     contentComponent: props => (
         <BrandedContainer size="md" lowerStyles={styles.lowerStyles}>
             <Text style={styles.welcome}>Welcome to Florida Pycon 2017!</Text>
